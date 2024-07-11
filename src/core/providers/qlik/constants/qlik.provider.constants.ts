0e9da36c5326725faccf5a8ef87f415a2a08dc80
Oscar <o.velilla@gmail.com>
2024-07-11 14:20:38 +0200

    updates

--- src/core/providers/qlik/constants/qlik.provider.constants.ts
+++ src/core/providers/qlik/constants/qlik.provider.constants.ts
@@ -1,7 +1,7 @@
 const HOST_CONFIG = {
   host: 'https://6l29pw1z4bpn8ap.eu.qlikcloud.com',
   clientId: 'a94a35663c22c4417cb9726d9ff70777',
-  redirectUri: 'https://localhost:5173/oauth-callback.html',
+  redirectUri: 'https://qlik-test.vercel.app/oauth-callback.html',
   accessTokenStorage: 'session',
   authType: 'Oauth2',
 };
