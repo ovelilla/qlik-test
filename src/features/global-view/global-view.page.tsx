4fbd4317f1acb0efc29090a838455d9fba6b8555
Oscar <o.velilla@gmail.com>
2024-07-11 14:17:54 +0200

    updates

--- src/features/global-view/global-view.page.tsx
+++ src/features/global-view/global-view.page.tsx
@@ -4,13 +4,13 @@ import { QlikEmbed } from '@qlik/embed-react';
 
 const GlobalViewPage = (): React.ReactElement => {
   const appId = 'dcfe13cf-c2c4-42ad-84fc-7510d179d154';
-  const sheetId = '3337e68d-c051-4f25-95e7-f4830baea0e4';
+  // const sheetId = '3337e68d-c051-4f25-95e7-f4830baea0e4';
   return (
     <div>
       <h1>Qlik Embed with React</h1>
-      <QlikEmbed ui="analytics/selections" appId={appId} />
+      {/* <QlikEmbed ui="analytics/selections" appId={appId} /> */}
       {/* <QlikEmbed ui="classic/app" app={appId} sheet={sheetId} /> */}
-      {/* <QlikEmbed ui="analytics/chart" appId={appId} objectId="SJQybjH" /> */}
+      <QlikEmbed ui="analytics/chart" appId={appId} objectId="SJQybjH" />
     </div>
   );
 };
